26d5b5159c1fdd4a512507e348eba1f5e39f786e
premkuamr <kumarprem75715@gmail.com>
2024-10-23 09:36:50 +0530

    changes done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const port = 5000;
